cb6bd72e995d9fc56a6f99f6c859576ad6d6ee27
PrabalVatsal01 <vatsalprabal@gmail.com>
2026-06-08 15:43:40 +0530

    Day 8 Assignment uploaded

---
+++ Day-8/Problem30.java
@@ -0,0 +1,11 @@
+public class Problem30 {
+    public static void main(String[] args) {
+        int rows = 5;
+        for(int i=1;i<=rows;i++){
+            for(int k=1;k<=i;k++){
+                System.out.print(k);
+            }
+            System.out.println(" ");    
+        }
+    }
+}
---
+++ Day-8/Problem31.java
@@ -0,0 +1,10 @@
+public interface Problem31 {
+    public static void main(String[] args) {
+        for(int i='A';i<='E';i++){
+            for(int k='A';k<=i;k++){
+                System.out.print((char)k);
+            }
+            System.out.println(" ");    
+        }
+    }
+}
