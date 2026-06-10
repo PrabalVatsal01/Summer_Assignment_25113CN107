897df736a5d6c1888e7e32bdfdb003107d45ad9b
PrabalVatsal01 <vatsalprabal@gmail.com>
2026-06-10 17:20:24 +0530

    Day 9 Assignment uploaded

---
+++ Day-9/Problem33.java
@@ -0,0 +1,8 @@
+public class Problem33 {
+    public static void main(String[] args) {
+    for(int i=1;i<=5;i++){
+        for(int j=0;j<=5-i;j++)
+            System.out.print("*");
+        System.out.println("");
+    }
+}}
---
+++ Day-9/Problem35.java
@@ -0,0 +1,11 @@
+public class Problem35 {
+    public static void main(String[] args) {
+        for(int i='A';i<='E';i++){
+            for(int j='A';j<=i;j++)
+            System.out.print((char)i);
+        System.out.println(" ");
+        }
+    }
+}
+
+
