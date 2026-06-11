42b546c6830225a8b2f666a4497dd2d1d1a2f568
PrabalVatsal01 <vatsalprabal@gmail.com>
2026-06-11 21:47:39 +0530

    Day-11 Assignment uploaded

---
+++ Day-11/Problem41.java
@@ -0,0 +1,7 @@
+// Program to Write function to find sum of two numbers 
+public class Problem41 {
+    static int sum(int n,int m){
+        return n+m;
+    }
+    
+}
---
+++ Day-11/Problem42.java
@@ -0,0 +1,9 @@
+//program to Write function to find maximum.
+public class Problem42 {
+    static int maximum(int n,int m){
+        return (m>n)?m:n;
+    }
+    public static void main(String[] args) {
+        System.out.println(maximum(4,9));
+    }
+}
